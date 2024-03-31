e6569b8b97c113aae018257c57a1d89e837db6fa
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-31 18:56:52 +0600

    home

---
+++ src/components/About/Home/Home.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+const Home = () => {
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log(location);
+    return (
+        <div>
+            <Header></Header>
+            {
+                navigation.state === "loading" ? 
+                <p>Loading...</p>: 
+                <Outlet></Outlet>
+            }
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
