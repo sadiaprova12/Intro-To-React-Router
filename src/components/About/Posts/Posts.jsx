05646acff97c9d8e295bb043ce274c802bac0964
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-31 18:57:27 +0600

    posts

---
+++ src/components/About/Posts/Posts.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import { useLoaderData } from "react-router-dom";
+import Post from "../Post/Post";
+
+
+const Posts = () => {
+    const posts = useLoaderData();
+    return (
+        <div>
+            <h2>Posts: {posts.length}</h2>
+            <div className="users">
+                {
+                    posts.map(post => <Post key={post.id} post={post}></Post>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Posts;
