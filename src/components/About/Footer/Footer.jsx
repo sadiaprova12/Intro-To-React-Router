5599a3280c3e766bf7a7a1eedd755c82c7853f7b
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-31 18:56:14 +0600

    footer

---
+++ src/components/About/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Thank  you for visiting our website</small></p>
+        </div>
+    );
+};
+
+export default Footer;
