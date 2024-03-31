396c74c2754448e9f7603fad4f29b58b2aa2b1ec
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-31 18:57:16 +0600

    post detailes

---
+++ src/components/About/PostDetailes/PostDetailes.jsx
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
+
+
+const PostDetails = () => {
+    const post = useLoaderData();
+    const {postId} = useParams();
+    const navigate = useNavigate();
+    const {id, title, body} = post;
+
+    console.log(postId);
+
+    const handleGoBack = () =>{
+        navigate(-1);
+    }
+
+    return (
+        <div>
+            <h3>POst details about: {id}</h3>
+            <p>Title: {title}</p>
+            <p><small>{body}</small></p>
+            <button onClick={handleGoBack}>Go back</button>
+        </div>
+    );
+};
+
+export default PostDetails;
