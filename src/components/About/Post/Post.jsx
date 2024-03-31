139472c18e700627cdcb6d59586a0608ee9db1d5
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-31 18:57:04 +0600

    post

---
+++ src/components/About/Post/Post.jsx
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import { Link, useNavigate } from "react-router-dom";
+
+
+// eslint-disable-next-line react/prop-types
+const Post = ({post}) => {
+    // eslint-disable-next-line react/prop-types
+    const {id, title} = post;
+
+    const navigate = useNavigate();
+
+    const postStyle = {
+        border: '2px solid yellow',
+        padding: '5px',
+        borderRadius: '20px'
+    }
+
+    const handleShowDetail = () =>{
+        navigate(`/post/${id}`);
+    }
+
+    return (
+        <div style={postStyle}>
+            <h4>Post of Id: {id}</h4>
+            <p>{title}</p>
+            <Link to={`/post/${id}`}>Post Detail</Link>
+            <Link to={`/post/${id}`}><button>Show Details</button></Link>
+            <button onClick={handleShowDetail}>Click to see details</button>
+        </div>
+    );
+};
+
+export default Post;
